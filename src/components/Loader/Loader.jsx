6d3e37a9291cbb290bf24990163be8679159117f
Monika Marciniak <monixxa90@gmail.com>
2022-11-25 14:55:05 +0100

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,16 @@
-import { Dna } from 'react-loader-spinner';
+import { ColorRing } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 export const Loader = () => (
   <div className={css.Loader}>
-    <Dna
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="dna-loading"
-      wrapperStyle={{}}
-      wrapperClass="dna-wrapper"
-    />
+    <ColorRing
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="blocks-loading"
+  wrapperStyle={{}}
+  wrapperClass="blocks-wrapper"
+  colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+/>
   </div>
 );
